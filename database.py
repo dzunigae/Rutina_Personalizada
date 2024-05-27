51f9a8433322e99ddf074fd106cdad2a7069b437
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2024-05-27 16:31:01 -0500

    Segunda iteración: Bases de datos

--- database.py
+++ database.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 DF_INFORMACION_EJERCICIOS = pd.read_excel('./Modelo.xlsx')
 
+DF_GRUPO = pd.DataFrame(columns=['Id', 'Nombre'])
+DF_IDENTIFICADOR_RUTINA = pd.DataFrame(columns=['Id', 'Indicador', 'Frecuencia'])
+DF_EJERCICIO = DF_GRUPO = pd.DataFrame(columns=['Id', 'Nombre', 'Nivel', 'Pagina', 'Repeticiones', 'Equipo', 'Peso', 'Tiempo', 'Frecuencia'])
+
 def trabajo_pd():
     print(DF_INFORMACION_EJERCICIOS)
 
