fd9439ee13c556cab6922aac9b84b546101fe971
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2024-12-14 19:48:36 -0500

    Iteración de construcción: 3

--- V1/index.py
+++ V1/index.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 
 # DEFINICIÓN DE OBJETOS
 
@@ -39,11 +40,6 @@ GRUPOS_MUSCULARES_LIST = []
 RUTINAS_LIST = []
 
 # DEFINICIÓN DE FUNCIONES
-
-# 5. Definición de función que construye la rutina de hoy (Lista de objetos de tipo rutina que se pueden hacer hoy :: RUTINAS):
-def construcción_de_la_rutina(lista_posibles_rutinas):
-    # Obtener la lista de músculos del día anterior
-    pass
 
 # MAIN
 
@@ -109,53 +105,130 @@ if __name__ == "__main__":
         rutina_actual = rutina(id,list_grupos_musculares)
         RUTINAS_LIST.append(rutina_actual)
 
+    # Variables
+    rutina_de_hoy = int(MODELO_DF['Rutina de hoy'].iloc[0])
+    diccionario_de_rutinas_por_valor = {
+        0:['A','B','C','D','E','F','G','H'],
+        1:['A','B','C','D','E','F','G','H'],
+        2:['I','J'],
+        3:'Descanso'
+    }
+    musculos_de_abdomen_excepcion_de_descanso = set() # Sólo los nombres, no todo el objeto
+    RUTINAS_FILTRADAS = []
+    musculos_en_descanso = MODELO_DF['Musculos en descanso'].dropna().tolist()
+    ejercicios_vetados = set()
+    valor_principiante = int(MODELO_DF['Principiante'].dropna().iloc[0])
+    valor_intermedio = int(MODELO_DF['Intermedio'].dropna().iloc[0])
+    valor_avanzado = int(MODELO_DF['Avanzado'].dropna().iloc[0])
+    plazas = []
+    intensidad_dict = {'Principiante': 1, 'Intermedio': 2, 'Avanzado': 3}
+    pares_rutina_frecuencia = dict()
+
     # PREPARAR LA RUTINA DE HOY
-    # xx. Sumar una unidad al valor de Rutina de hoy, en caso de ser 3, será una rutina de tren inferior, en caso de
-    # ser menor, será una de tren superior, en caso de ser 4, es un día de descanso y se retorna el valor a 0
-    # xx. Recordar que los músculos del abdomen no necesitan periodos de descanso, añadir la variable de excepción
-    # xx. Revisar que músculos están en descanso y eliminarlos de todas las listas de los grupos musculares 
-    # xx. Revisar que ejercicios están relacionados a los músculos en descanso y eliminarlos de las listas de ejercicios de los músculos restantes
-    # ALGORITMO ESPECIAL PARA SEPARAR LAS PLAZAS DE LOS EJERCICIOS Y ESCOGER LOS PROPIOS EJERCICIOS
-    # xx. Repetir esto para las 3 plazas de ejercicios
-        # xx. valor_general = principante+intermedio+avanzado
-        # xx. Generar un número aleatorio entre 0 y valor_general
-        # xx. Se escoge el valor de intensidad(principante,intermedio,avanzado) que tenga el menor valor numérico,
-        #     Si el número aleatorio va de 0 al menor valor, se escoge ese menor valor
-        # xx. Hacer lo mismo pero con los siguientes rangos:
-        #     menor valor - valor intermedio
-        #     valor intermedio - mayor valor
-    # NOTA: Las plazas deben ser listas o tuplas que contengan su id (momento secuencial en el cual saldrá el ejercicio
-    # relacionado), su intensidad y un espacio para poner finalmente el ejercicio que se realizará, inicialmente vacío.
-    # LAS PLAZAS YA ESTÁN CREADAS, AHORA A ESCOGER LOS EJERCICIOS
-    # xx. Se ordenan las plazas desde la que tiene mayor intensidad hasta la de menor intensidad
-    # xx. A cada rutina le asignamos un valor dependiendo de la suma de las frecuencias con que hayan sido 
-    # ejercitados sus grupos musculares. (Para priorizar aquellas cuyos grupos musculares se hayan ejercitado menos)
-    # xx. Ordenamos la lista de rutinas desde la de menor valor anterior hasta el mayor
-    # xx. for rutina in lista_rutinas:
-        # xx. lista_grupo_muscular_actual = rutina.lista_grupos_musculares
-        # xx. Verificar si cada grupo muscular de lista_grupo_muscular_actual tiene por lo menos un músculo libre para entrenar
-        # En caso de que no, se procede a la siguiente iteración.
-        # xx. Se crea una nueva estructura de datos que contiene la siguiente información:
-        # Tuplas con nombre del grupo muscular, nombre de músculo, ejercicio e intensidad. 
-        # El objetivo es que estas tuplas vayan siendo obtenidas en un orden específico.
-        # xx. Se ordenan de menor frecuencia a mayor frecuencia todos los grupos musculares presentes en la rutina.
-        # xx. Para cada uno de estos grupos musculares, se ordena su lista de músculos también de menor a mayor frecuencia.
-        # xx. Para cada uno de los musculos se ordena de menor a mayor frecuencia sus ejercicios asociados
-        # y se crea la tupla con la información y se añade a la lista con un append
-        # xx. for plaza in plazas_ordenadas:
-            # xx. Se busca la primera tupla que cumpla con el requisito de intensidad de la plaza
-            # xx. Si se encuentra:
-                # xx. La plaza queda asociada al ejercicio en cuestión.
-                # xx. Se da por ejercitado el grupo muscular y se eliminan las tuplas de la lista que tengan ese grupo muscular.
-                # xx. También se eliminan de la estructura de datos de tuplas todas aquellas que repitan el ejercicio que acaba de salir.
-            # xx. Si no se encuentra, se cambia la intensidad de esa plaza a su intensidad inmediantamente menor y se realiza nuevamente la búsqueda.
-            # xx. Si en la intensidad más baja no se encuentra cómo rellenar la plaza, se salta a verificar la siguiente rutina.
-        # xx. Si al final todas las plazas fueron llenadas, se da por concluído el proceso.
-        # xx. Si queda alguna plaza por llenar, verificar si es porque todos los grupos musculares ya fueron sacados de las posibilidades.
-        # En dado caso, se reinicia el proceso para llenar las plazas faltantes.
-    # xx. Si el for de rutinas termina y hay plazas que no fueron llenadas, se declara el día como de descanso anticipado.
-    # xx. Si todas las plazas están llenadas, se reemplazan los músculos a descansar y se actualizan todas las demás variables necesarias.
-    # xx. Se crea un txt con la rutina.
-    # xx. Se da espacio a esperar que la rutina culmine para dar la retroalimentación:
-        # xx. Si la rutina fue hecha satisfactoriamente, se realizan las modificaciones necesarias en las probabilidades de las intensidades.
-        # xx. Si no, se realiza la modificación contraria a las probabilidades para bajar el nivel de la siguiente rutina.
+    # xx. Sumar una unidad al valor de Rutina de hoy, en caso de ser 2, será una rutina de tren inferior, en caso de
+    # ser menor, será una de tren superior, en caso de ser 3, es un día de descanso y se retorna el valor a 0
+    valor_rutina_de_hoy = diccionario_de_rutinas_por_valor[rutina_de_hoy]
+    if valor_rutina_de_hoy != 'Descanso':
+        rutina_de_hoy = rutina_de_hoy+1
+        # xx. Filtrar las rutinas con índices válidos
+        RUTINAS_FILTRADAS = [rt for rt in RUTINAS_LIST if rt.index in valor_rutina_de_hoy]
+        # xx. Recordar que los músculos del abdomen no necesitan periodos de descanso, añadir la variable de excepción
+        grupo_muscular_abdomen = next((gm for gm in GRUPOS_MUSCULARES_LIST if gm.nombre == 'Abdomen'), None)
+        for mus in grupo_muscular_abdomen.lista_musculos:
+            musculos_de_abdomen_excepcion_de_descanso.add(mus.nombre)
+        # xx. Revisar que músculos están en descanso y eliminarlos de todas las listas de los grupos musculares 
+        for gp in GRUPOS_MUSCULARES_LIST:
+            gp.lista_musculos = [
+                musculo for musculo in gp.lista_musculos
+                if musculo.nombre not in musculos_en_descanso
+            ]
+        # xx. Revisar que ejercicios están relacionados a los músculos en descanso y eliminarlos de las listas de ejercicios de los músculos restantes
+        # Recorremos la lista de músculos extrayendo los nombres de los ejercicios
+        for mus in MUSCULOS_LIST:
+            if mus.nombre in musculos_en_descanso:
+                for ej in mus.lista_ejercicios:
+                    ejercicios_vetados.add(ej.nombre)
+        # Eliminamos de las listas de músculos todos los ejercicios vetados
+        for mus in MUSCULOS_LIST:
+            mus.lista_ejercicios = [
+                ejer for ejer in mus.lista_ejercicios
+                if ejer.nombre not in ejercicios_vetados
+            ]
+        # ALGORITMO ESPECIAL PARA SEPARAR LAS PLAZAS DE LOS EJERCICIOS Y ESCOGER LOS PROPIOS EJERCICIOS
+        # xx. Repetir esto para las 3 plazas de ejercicios
+        for plaza in range(3):
+            intensidad = ''
+            # xx. valor_general = principante+intermedio+avanzado
+            valor_general = valor_principiante+valor_intermedio+valor_avanzado
+            # xx. Generar un número aleatorio entre 1 y valor_general
+            entero_aleatorio = random.randint(1, valor_general)
+            # xx. Escoger la intensidad relacionada a la plaza
+            if 0 < entero_aleatorio <= valor_principiante:
+                intensidad = 'Principiante'
+            elif valor_principiante < entero_aleatorio <= valor_principiante+valor_intermedio:
+                intensidad = 'Intermedio'
+            elif valor_principiante+valor_intermedio < entero_aleatorio <= valor_general:
+                intensidad = 'Avanzado'
+            # NOTA: Las plazas deben ser listas que contengan su id (momento secuencial en el cual saldrá el ejercicio
+            # relacionado), su intensidad y un espacio para poner finalmente el ejercicio que se realizará, inicialmente vacío.
+            plazas.append([plaza+1,intensidad,None])
+        # LAS PLAZAS YA ESTÁN CREADAS, AHORA A ESCOGER LOS EJERCICIOS
+        # xx. Se ordenan las plazas desde la que tiene mayor intensidad hasta la de menor intensidad
+        # Ordenar la lista usando la intensidad como clave
+        plazas = sorted(plazas, key=lambda x: intensidad_dict[x[1]], reverse=True)
+        # xx. A cada rutina le asignamos un valor dependiendo de la suma de las frecuencias con que hayan sido 
+        # ejercitados sus grupos musculares. (Para priorizar aquellas cuyos grupos musculares se hayan ejercitado menos)
+        for rut in RUTINAS_FILTRADAS:
+            g_musculares = [
+                gp.nombre for gp in rut.lista_grupos_musculares
+            ]
+            lista_frecuencias_de_grupos_musculares = MODELO_DF[g_musculares].iloc[0].to_list()
+            pares_rutina_frecuencia[rut.index] = sum(lista_frecuencias_de_grupos_musculares)
+        # xx. Ordenamos el dict de rutinas desde la de menor valor anterior hasta el mayor
+        rutinas_ordenadas = sorted(pares_rutina_frecuencia.items(), key=lambda item: item[1])
+        # xx. for rutina in lista_rutinas:
+        for rutina_ordenada in rutinas_ordenadas:
+            rutina_actual = [obj for obj in RUTINAS_FILTRADAS if obj.index == rutina_ordenada[0]][0]
+            # xx. lista_grupo_muscular_actual = rutina.lista_grupos_musculares
+            lista_grupo_muscular_actual = rutina_actual.lista_grupos_musculares
+            # xx. Verificar si cada grupo muscular de lista_grupo_muscular_actual tiene por lo menos un músculo libre para entrenar
+            # En caso de que no, se procede a la siguiente iteración.
+            saltar_a_la_siguiente = False
+            for gp in lista_grupo_muscular_actual:
+                if len(gp.lista_musculos) < 1:
+                    saltar_a_la_siguiente = True
+                    break
+            if saltar_a_la_siguiente:
+                continue
+            # xx. Se crea una nueva estructura de datos que contiene la siguiente información:
+            # Tuplas con nombre del grupo muscular, nombre de músculo, ejercicio e intensidad. 
+            # El objetivo es que estas tuplas vayan siendo obtenidas en un orden específico.
+            # xx. Se ordenan de menor frecuencia a mayor frecuencia todos los grupos musculares presentes en la rutina.
+            # xx. Para cada uno de estos grupos musculares, se ordena su lista de músculos también de menor a mayor frecuencia.
+            # xx. Para cada uno de los musculos se ordena de menor a mayor frecuencia sus ejercicios asociados
+            # y se crea la tupla con la información y se añade a la lista con un append
+            # xx. for plaza in plazas_ordenadas:
+                # xx. Se busca la primera tupla que cumpla con el requisito de intensidad de la plaza
+                # xx. Si se encuentra:
+                    # xx. La plaza queda asociada al ejercicio en cuestión.
+                    # xx. Se da por ejercitado el grupo muscular y se eliminan las tuplas de la lista que tengan ese grupo muscular.
+                    # xx. También se eliminan de la estructura de datos de tuplas todas aquellas que repitan el ejercicio que acaba de salir.
+                # xx. Si no se encuentra, se cambia la intensidad de esa plaza a su intensidad inmediantamente menor y se realiza nuevamente la búsqueda.
+                # xx. Si en la intensidad más baja no se encuentra cómo rellenar la plaza, se salta a verificar la siguiente rutina.
+            # xx. Si al final todas las plazas fueron llenadas, se da por concluído el proceso.
+            # xx. Si queda alguna plaza por llenar, verificar si es porque todos los grupos musculares ya fueron sacados de las posibilidades.
+            # En dado caso, se reinicia el proceso para llenar las plazas faltantes.
+        # xx. Si el for de rutinas termina y hay plazas que no fueron llenadas, se declara el día como de descanso anticipado.
+        # xx. Si todas las plazas están llenadas, se reemplazan los músculos a descansar y se actualizan todas las demás variables necesarias.
+        # xx. Se crea un txt con la rutina.
+        # xx. Se da espacio a esperar que la rutina culmine para dar la retroalimentación:
+            # xx. Si la rutina fue hecha satisfactoriamente, se realizan las modificaciones necesarias en las probabilidades de las intensidades.
+            # xx. Si no, se realiza la modificación contraria a las probabilidades para bajar el nivel de la siguiente rutina.
+    # Opción de día de descanso
+    else:
+        pass
+        # xx. Vaciar la columna de músculos en descanso
+        # xx. Actualizar la variable de rutina de hoy
+
+
+#print([objeto.index for objeto in RUTINAS_FILTRADAS])
